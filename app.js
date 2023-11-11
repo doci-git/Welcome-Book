6e2c786ffc4b60f319380bed8649bd4125c4ec99
Musa Doci <81824102+doci-git@users.noreply.github.com>
2023-11-11 20:07:30 +0100

    add js hide and show

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+// css library AOS
 AOS.init();
-
+// nav menu
 const navItems = document.getElementsByClassName("nav-item");
 
 for (let i = 0; i < navItems.length; i++) {
@@ -9,4 +10,28 @@ for (let i = 0; i < navItems.length; i++) {
 
     navItems[i].classList.add("active");
   });
+}
+
+//select section hide and show
+let divs = ["section1", "section2", "section3"];
+let visibleId = null;
+
+function show(id) {
+  if (visibleId !== id) {
+    visibleId = id;
+  }
+  hide();
+}
+
+function hide() {
+  let div, i, id;
+  for (i = 0; i < divs.length; i++) {
+    id = divs[i];
+    div = document.getElementById(id);
+    if (visibleId === id) {
+      div.style.display = "block";
+    } else {
+      div.style.display = "none";
+    }
+  }
 }
